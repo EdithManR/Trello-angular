ecce38c40436280f760f832def5e249e45dde17e
EdithMR <edithmandujano001@gmail.com>
2026-01-17 22:16:14 -0600

    Se agrega primeng v20

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
@@ -14,6 +15,7 @@ export const appConfig: ApplicationConfig = {
       theme: {
           preset: Aura
       }
-    })
+    }),
+    provideAnimationsAsync(),
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { ButtonModule } from 'primeng/button';
+
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule ],
+  imports: [RouterOutlet, ButtonModule, ToastModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers:[MessageService]
 })
 export class App {
   protected readonly title = signal('Trello-angular');
+  constructor(private messageService: MessageService) {}
+
+    show() {
+        this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Message Content', life: 3000 });
+  }
 }
